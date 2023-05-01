00d589a34e6b7d11d5bf55f9086d0b4efe2f05ea
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-05-01 11:05:00 +0200

    feat(projectView): adding responsive

--- frontend/src/views/Project/ProjectView.tsx
+++ frontend/src/views/Project/ProjectView.tsx
@@ -1,14 +1,15 @@
-import { Box, Chip, Typography } from "@mui/material";
+import { Box, Chip, Typography, useMediaQuery } from "@mui/material";
 import { projectByYears } from "../../utils/projects";
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { project } from "../../utils/projects";
-import IconButton from '@mui/material/IconButton';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LanguageIcon from '@mui/icons-material/Language';
+import IconButton from "@mui/material/IconButton";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LanguageIcon from "@mui/icons-material/Language";
 
 export default function ProjectView() {
   const [project, setProject] = useState<project>({} as project);
+  const isSmallScreen = useMediaQuery("(max-width:1000px)");
 
   const params = useParams();
 
@@ -23,64 +24,66 @@ export default function ProjectView() {
   }, [params.title]);
 
   return (
-    
-<div>
-  <Typography variant="h4" align="center" gutterBottom sx={{fontWeight: "bold"}}> 
-    {project.name}
-  </Typography>
-  <Box display="flex" alignItems="center" margin={"50px"}>
-    <Box mr={20}>
+    <div>
+      <Typography variant="h4" align="center" gutterBottom sx={{ fontWeight: "bold" }}>
+        {project.name}
+      </Typography>
+      <Box display="flex" alignItems="center" margin={"50px"} sx={{ display: isSmallScreen ? "inline" : "flex" }}>
+        <Box mr={20} sx={{ display: isSmallScreen ? "none" : "inline" }}>
+          <Typography variant="subtitle1" sx={{ fontSize: "1.2em", textAlign: "left" }} gutterBottom>
+            <b>Description: </b>
+            <br />
 
-      <Typography variant="subtitle1" sx={{fontSize: "1.2em", textAlign: "left"}} gutterBottom>
-        <b>Description: </b>
-        <br />
-    
-        {project.description}
-      </Typography>
-      <br />
-     
-      
-      <Typography variant="subtitle1" sx={{fontSize: "1.2em"}} gutterBottom>
-        Tags:{" "}
-        {project.tags?.map((tag, index) => {
-          return (
-            <Chip
-              key={index}
-              label={tag}
-              color="primary"
-              sx={{ marginRight: "10px" }}
-            />
-          );
-        })}
-      </Typography>
-     
-    </Box>
+            {project.description}
+          </Typography>
+          <br />
 
-    <Box>
-      <img src={project.thumbnailUrl} alt={project.name} style={{width: "45vw", borderRadius: "10px", marginTop: "30px"}}/>
-      <Box sx={{display: "flex", justifyContent: "center"}}>
-      {project.prodUrl && (
-        <Typography variant="subtitle1" sx={{fontSize: "1.2em"}} gutterBottom>
-        Production url: 
-        <IconButton href={project.prodUrl} target="_blank" rel="noopener noreferrer">
-          <LanguageIcon sx={{color: "white", fontSize: "40px"}}/>
-        </IconButton>
-      </Typography>
-      )}
-      {project.sourceUrl && (
-        <Typography variant="subtitle1" sx={{fontSize: "1.2em"}} gutterBottom>
-          Source code: 
-          <IconButton href={project.sourceUrl} target="_blank" rel="noopener noreferrer">
-            <GitHubIcon sx={{color: "white", fontSize: "40px"}}/>
-          </IconButton>
-        </Typography>
-      )}
+          <Typography variant="subtitle1" sx={{ fontSize: "1.2em" }} gutterBottom>
+            Tags:{" "}
+            {project.tags?.map((tag, index) => {
+              return <Chip key={index} label={tag} color="primary" sx={{ marginRight: "10px" }} />;
+            })}
+          </Typography>
+        </Box>
+
+        <Box>
+          <img src={project.thumbnailUrl} alt={project.name} style={{ width: "45vw", borderRadius: "10px",  minWidth: "400px", margin: "40px" }} />
+          <Box mr={isSmallScreen ? 0 : 20} sx={{ display: isSmallScreen ? "flex" : "none", flexDirection: "column", alignItems: "center" }}>
+              <Typography variant="subtitle1" sx={{ fontSize: "1.2em", textAlign: "left", margin: "40px" }} gutterBottom>
+                <b>Description: </b>
+                <br />
+
+                {project.description}
+              </Typography>
+              <br />
+
+              <Typography variant="subtitle1" sx={{ fontSize: "1.2em", margin: "40px" }} gutterBottom>
+                Tags:{" "}
+                {project.tags?.map((tag, index) => {
+                  return <Chip key={index} label={tag} color="primary" sx={{ marginRight: "10px" }} />;
+                })}
+              </Typography>
+            </Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            {project.prodUrl && (
+              <Typography variant="subtitle1" sx={{ fontSize: "1.2em" }} gutterBottom>
+                Production url:
+                <IconButton href={project.prodUrl} target="_blank" rel="noopener noreferrer">
+                  <LanguageIcon sx={{ color: "white", fontSize: "40px" }} />
+                </IconButton>
+              </Typography>
+            )}
+            {project.sourceUrl && (
+              <Typography variant="subtitle1" sx={{ fontSize: "1.2em" }} gutterBottom>
+                Source code:
+                <IconButton href={project.sourceUrl} target="_blank" rel="noopener noreferrer">
+                  <GitHubIcon sx={{ color: "white", fontSize: "40px" }} />
+                </IconButton>
+              </Typography>
+            )}
+          </Box>
+        </Box>
       </Box>
-    </Box>
-
-  </Box>
-</div>
-  
-
+    </div>
   );
 }
